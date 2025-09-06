0014046c1722a07f946fbb75aa824ed403d7e259
mayanktiwari-1827 <editverse1827@gmail.com>
2025-09-06 15:56:35 +0530

    Mp_change

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,5 @@
 import { HeaderShownContext } from "@react-navigation/elements";
 import { Stack } from "expo-router";
-import {StatusBar} from "expo-status-bar"
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
